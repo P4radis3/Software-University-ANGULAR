23bd58c894e4e5a441cf755a1ff01ff1d1ec6bc4
Yordan Tabakov <112849433+P4radis3@users.noreply.github.com>
2024-12-30 18:17:31 +0200

    Implement Footer
    Introduced a reusable footer across the application, improving the structure and user interface uniformity.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { HomeComponent } from './home/home.component';
-import { TasksModule } from './tasks/tasks.module';
-import { ErrorComponent } from './error/error.component';
-import { UserModule } from './user/user.module';
-import { appInterceptorProvider } from './app.interceptor';
-import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UserModule } from './user/user.module';
+
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { FooterComponent } from "./core/footer/footer.component";
+
+import { appInterceptorProvider } from './app.interceptor';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ErrorComponent, AuthenticateComponent],
-  imports: [ BrowserModule, BrowserAnimationsModule, CoreModule, SharedModule, TasksModule, UserModule, HttpClientModule, AppRoutingModule, MatDialogModule, MatSnackBarModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MatDialogModule, MatSnackBarModule, CoreModule, SharedModule, TasksModule, UserModule, AppRoutingModule, FooterComponent],
   providers: [appInterceptorProvider],
   bootstrap: [AppComponent]
 })
--- client/src/app/core.module.ts
+++ client/src/app/core.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-
-
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
